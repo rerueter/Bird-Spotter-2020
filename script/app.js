3cb77f060e294761774517df75190d67e9d00151
rerueter <rerueter@gmail.com>
2020-01-08 11:45:05 -0800

    adds intro splash, code refinements to num generation, etc

--- script/app.js
+++ script/app.js
@@ -1,6 +1,10 @@
 console.log('it a js')
+//variables for adjusting the dials//
+
 //game object
 const game = {
+  size:100,
+  time:30,
   cover:['<i class="rock fas fa-mountain"></i>','<i class="tree fas fa-tree"></i>','<i class="bush fas fa-spa"></i>'],
   p1:{
     score:0,
@@ -23,12 +27,16 @@ const buildField=(size)=>{
     $('#game_field').append(`<div class="tile">${coverType}</div>`)
   };
 };
-buildField(100);
+buildField(game.size)
+
+//===coord gen for putting birds on things===//
+locator =(num)=>{
+  return Math.floor(Math.random()*num)+1; //rndm based on num input
+}
 
 //=== put a bird on (behind) it===//
-const paboi =()=>{
-  
-  const position = Math.floor(Math.random()*100); //0-99 corresponding to tile index numbers
+const paboi =(num)=>{
+  const position = num;
   const $target = $('.tile').eq(position).addClass('target'); //select .tile element
   const $cover_active = $($target).children().eq(0); //select cover element
   
@@ -46,31 +54,36 @@ const paboi =()=>{
 }
 
 //===game clock===//
-let count = 30;
-
 const tick=()=>{
   paboi();
   console.log(count);
-  document.getElementById('year').innerHTML= `${count}`; ////!TEMP! CHANGE THIS////
+  document.getElementById('year').innerHTML= `${game.time}`; ////!TEMP! CHANGE THIS////
   if(count<=0){
     clearInterval(gameClock);
     document.getElementById('year').innerHTML= `Time!`;
     return
   };
-  count -= 1;
+  game.time -= 1;
 };
 
 const startClock=()=>{
   gameClock = setInterval(tick,1000);
 }
-//===game clock===//
+//===Bird Bouncer===//
+const birdBounce=()=>{  
+    $('.crow').toggleClass(`animated bounce infinite`);
+    $('.dove').toggleClass(`animated bounce infinite`);
+    $('.kiwi').toggleClass(`animated bounce infinite`);
+}
 
-// animation tester - getting to grips w animate css
-$('#btn-animate').on('click', ()=>{
-  $('.crow').toggleClass(`animated bounce infinite`);
-  $('.dove').toggleClass(`animated bounce infinite`);
-  $('.kiwi').toggleClass(`animated bounce infinite`);
-});
+//===Listeners===//
+// start button
+$('#start').on('click', ()=>{
+  $('#splash').toggleClass('slideOutUp');
+})
+// bird bouncer
+$('#btn-animate').on('click', birdBounce);
+// clock listener
 $('#btn-timer').on('click', startClock);
 
 // bird click listener
@@ -89,13 +102,7 @@ $('body').on('click', '.bird', ()=>{
 
 // actually a nightmare, not fun. 
 const funBirds =()=>{
-  for(let i=0; i<100; i+=3){
-    if($('tile').eq(i).children().eq(0).hasClass('rock')){
-      $('.tile').eq(i).append(`<i class="bird crow fas fa-crow"></i>`)
-    }else if($('.tile').eq(i).children().eq(0).hasClass('bush')){
-      $('.tile').eq(i).append(`<i class="bird dove fas fa-dove"></i>`)
-    }else if($('.cover').eq(i).children().eq(0).hasClass('tree')){
-      $('.tile').eq(i).append(`<i class="bird kiwi fas fa-kiwi-bird"></i>`)
-    }
-  }
+ for(let i=1;i<game.length;i+=locator(4)){
+   paboi(i);
+ }
 }
